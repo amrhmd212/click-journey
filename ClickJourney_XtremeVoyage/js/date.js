d19b696457aed33d466005eb557402d754862523
Kiretas <a.amrioui20@gmail.com>
2025-05-18 23:54:24 +0200

    Delete ClickJourney_XtremeVoyage directory

--- ClickJourney_XtremeVoyage/js/date.js
+++
@@ -1,4 +0,0 @@
-
-    const today = new Date().toISOString().split('T')[0];
-    document.getElementById('departure-date').setAttribute('min', today);
-    document.getElementById('return-date').setAttribute('min', today);
